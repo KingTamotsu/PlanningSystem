e06e0e5b1cba4661f747f27fccf8bcca35f281b4
Jeannot Thewissen <1614614thewissen@zuyd.nl>
2018-10-24 14:17:29 +0200

    Controller, model and view added.

---
+++ PlanningSystem/Controllers/CourseController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PlanningSystem.Controllers
+{
+    public class CourseController : Controller
+    {
+        // GET: Course
+        public ActionResult Overview()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ PlanningSystem/Models/Course.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PlanningSystem.Models
+{
+    public class Course
+    {
+        public int courseId;
+        public string courseCode;
+        public string courseName;
+        public string description;
+        public List<string> lectures;
+    }
+}
