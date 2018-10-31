e337dd8c4f5626300bc98b5a15715cbc1380212f
Mike Jarratt <1240846jarratt@zuyd.nl>
2018-10-31 14:06:42 +0100

    Added unavailability class, controller + 2 views

---
+++ PlanningSystem/Controllers/UnavailabilityController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PlanningSystem.Models;
+
+namespace PlanningSystem.Controllers
+{
+    public class UnavailabilityController : Controller
+    {
+        public ActionResult Formulier()
+        {
+            ViewBag.Message = "Geef hier onbeschikbaarheid op.";
+
+            return View();
+        }
+        public ViewResult Respons(DateTime date, string startTijdUur, string startTijdMinuten, string eindTijdUur, string eindTijdMinuten, string reden)
+        {
+            var context = new PlanningSysteemEntities();
+            var newUnavailability = new Unavailability();
+            newUnavailability.UnavailabilityCause = reden;
+            newUnavailability.UnavailabilityStartTime = new DateTime(date.Year, date.Month, date.Day, Convert.ToInt16(startTijdUur), Convert.ToInt16(startTijdMinuten), 0);
+            newUnavailability.UnavailabilityEndTime = new DateTime(date.Year, date.Month, date.Day, Convert.ToInt16(eindTijdUur), Convert.ToInt16(eindTijdMinuten), 0);
+            newUnavailability.userID = 1;
+            context.SaveChanges();
+            return View(newUnavailability);
+        }
+    }
+}
---
+++ PlanningSystem/Models/Unavailability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PlanningSystem.Models
+{
+    public class Unavailability
+    {
+        public DateTime UnavailabilityStartTime { get; set; }
+        public DateTime UnavailabilityEndTime { get; set; }
+        public string UnavailabilityCause { get; set; }
+        public int userID { get; set; }
+    }
+}
