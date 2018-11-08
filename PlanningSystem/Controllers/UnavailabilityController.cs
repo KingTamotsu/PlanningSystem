deb5c47f78a9f775595e3cfa9bdc094200762ef7
Mike Jarratt <1240846jarratt@zuyd.nl>
2018-11-08 10:02:46 +0100

    translated remaing dutch words into english, added some comments in unavailabilitycontroller

--- PlanningSystem/Controllers/UnavailabilityController.cs
+++ PlanningSystem/Controllers/UnavailabilityController.cs
@@ -9,19 +9,35 @@ namespace PlanningSystem.Controllers
 {
     public class UnavailabilityController : Controller
     {
+        /// <summary>
+        /// This loads the view in which a user can register an unavailability, which is a partial or whole day in which
+        /// he/her is unavailable for any school related tasks
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Formulier()
         {
             ViewBag.Message = "Geef hier onbeschikbaarheid op.";
 
             return View();
         }
-        public ViewResult Respons(DateTime date, string startTijdUur, string startTijdMinuten, string eindTijdUur, string eindTijdMinuten, string reden)
+        /// <summary>
+        /// This processes the values that the user has declared and registers them into the database.
+        /// It then loads a response view with either a conformation or an error message.
+        /// </summary>
+        /// <param name="date"></param>
+        /// <param name="startTimeHour"></param>
+        /// <param name="startTimeMinutes"></param>
+        /// <param name="endTimeHour"></param>
+        /// <param name="endTimeMinutes"></param>
+        /// <param name="cause"></param>
+        /// <returns></returns>
+        public ViewResult Respons(DateTime date, string startTimeHour, string startTimeMinutes, string endTimeHour, string endTimeMinutes, string cause)
         {
             var context = new PlanningSysteemEntities();
             var newUnavailability = new Unavailability();
-            newUnavailability.UnavailabilityCause = reden;
-            newUnavailability.UnavailabilityStartTime = new DateTime(date.Year, date.Month, date.Day, Convert.ToInt16(startTijdUur), Convert.ToInt16(startTijdMinuten), 0);
-            newUnavailability.UnavailabilityEndTime = new DateTime(date.Year, date.Month, date.Day, Convert.ToInt16(eindTijdUur), Convert.ToInt16(eindTijdMinuten), 0);
+            newUnavailability.UnavailabilityCause = cause;
+            newUnavailability.UnavailabilityStartTime = new DateTime(date.Year, date.Month, date.Day, Convert.ToInt16(startTimeHour), Convert.ToInt16(startTimeMinutes), 0);
+            newUnavailability.UnavailabilityEndTime = new DateTime(date.Year, date.Month, date.Day, Convert.ToInt16(endTimeHour), Convert.ToInt16(endTimeMinutes), 0);
             newUnavailability.userID = 3;
             context.Unavailability.Add(newUnavailability);
             var addedUnavailability = new Models.Unavailability
@@ -44,15 +60,18 @@ namespace PlanningSystem.Controllers
                 return View(addedUnavailability);
 
             }
-
-
-
         }
 
-        public ViewResult Inzien()
+        /// <summary>
+        /// This loads the view containing a weekly overview of all unavailabilities of a user
+        /// </summary>
+        /// <param name="week"></param>
+        /// <returns></returns>
+        public ViewResult Inzien(int week = 1)
         {
             PlanningSysteemEntities context = new PlanningSysteemEntities();
-            DateTime monday = new DateTime(2018, 11, 5, 9, 0, 0);
+            DateTime startDay = new DateTime(2018, 9, 10, 9, 0, 0);
+            DateTime monday = startDay.AddDays(7*(week-1));
 
             List<List<Models.Unavailability>> totalList = new List<List<Models.Unavailability>>();
             List<Models.Unavailability> mondayUnavailabilities = new List<Models.Unavailability>();
